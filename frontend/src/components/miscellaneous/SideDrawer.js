187233691168e5e94bd7861692c084c552afa548
blazing-storm <mayankagrawal.main@gmail.com>
2022-06-19 00:58:34 +0545

    ChatPage Body: MyChats i.e List of Chats

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -13,6 +13,7 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
+  Spinner,
   Text,
   Tooltip,
   useDisclosure,
@@ -33,7 +34,7 @@ const SideDrawer = () => {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
-  const { user } = ChatState();
+  const { user, setSelectedChat, chats, setChats } = ChatState();
   const navigate = useNavigate();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
@@ -80,8 +81,33 @@ const SideDrawer = () => {
     }
   };
 
-  const accessChat = userId => {
-    //
+  const accessChat = async userId => {
+    try {
+      setLoadingChat(true);
+      const config = {
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.post('/api/chat', { userId }, config);
+
+      if (!chats.find(c => c._id === data._id)) setChats([data, ...chats]);
+
+      setSelectedChat(data);
+      setLoading(false);
+      onClose();
+    } catch (err) {
+      toast({
+        title: 'Error fetching the chat',
+        description: err.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-left',
+      });
+    }
   };
 
   return (
@@ -162,6 +188,7 @@ const SideDrawer = () => {
                 />
               ))
             )}
+            {loadingChat && <Spinner ml="auto" display="flex" />}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
